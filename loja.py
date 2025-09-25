c1595206587d27f8bad2e9d1e76efadcd2b77f4e
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2025-09-25 10:32:06 -0300

    Add bar chart examples and fix matplotlib import
    Added bar_colors.py to demonstrate individual bar colors in matplotlib bar charts. Updated loja.py to fix the matplotlib import and added code to visualize total sales per game using a bar chart.

--- loja.py
+++ loja.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 # carregar planilha do excel
 try:
     #df=pd.read_excel('vendas.xlsx')
@@ -14,4 +14,15 @@ df['total_vendas']=df['quantidade']*df['valor']
 #agrupar os dados por jogo e somar
 vendas_por_jogo=df.groupby('jogo')['total_vendas'].sum().reset_index()
 print("Total vendas por jogo: ",vendas_por_jogo)
+#Visualização das vendas
+#Criação de um gráfico
+plt.figure(figsize=(10,6))
+plt.bar(vendas_por_jogo['jogo'],vendas_por_jogo['total_vendas'],color='blue')
+plt.title('Total de vendas por jogo')
+plt.xlabel('Jogo')
+plt.ylabel('Total de Vendas R$')
+plt.xticks(rotation=45)
+plt.grid(axis='y',linestyle='--')
+plt.tight_layout()
+plt.show()
 
